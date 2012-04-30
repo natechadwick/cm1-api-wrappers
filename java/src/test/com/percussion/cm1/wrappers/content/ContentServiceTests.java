1f262fb1c8260ad90fdf2e7c89a194c93404c578
Nate Chadwick <nate.chadwick@gmail.com>
2012-04-30 12:11:15 -0400

    Initial checkin

---
+++ java/src/test/com/percussion/cm1/wrappers/content/ContentServiceTests.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+ *
+ * [ ContentServiceTests.java ]
+ * 
+ * COPYRIGHT (c) 1999 - 2012 by Percussion Software, Inc., Woburn, MA USA.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Percussion.
+ *
+ ******************************************************************************/
+package com.percussion.cm1.wrappers.content;
+
+/**
+ * @author natechadwick
+ *
+ */
+public class ContentServiceTests {
+
+}
